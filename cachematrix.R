f34aec6eebce333262e7c613f5af10300a5943a3
Tarik2811 <34143227+Tarik2811@users.noreply.github.com>
2018-01-14 10:47:50 -0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,80 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeVector function creates vector
 
-## Write a short comment describing this function
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  # vector value setting
+  set <- function(y) {    
+    x <<- y
+    m <<- NULL
+  }
+  # get vector value
+   get <- function() x
+  # Set mean value 
+  setmean <- function(mean) m <<- mean
+  # Get mean value
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+# cachemean function created with the function stated previously
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+#Chedk if the mean calculated---> retrurn mean
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+
+# if mean not calculated (calculate data mean) 
+  m <- mean(data, ...)
+ 
+# Set mean value  
+  x$setmean(m)
+  m
+}
+
+## function that cache the inverse of a matrix.
+## Function to create "matrix" that caches its inverse.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Function to compute the inverse of the matrix gotten from makeCacheMatrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return inverse matrix of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
 }
+
+
+
+
+
+
+  
+  
+
+
+
